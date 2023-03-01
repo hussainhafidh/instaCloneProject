84c75e94f8250d446bc2eddffa36aaf20c3a2cd2
Hussain <hussainhafidh1@gmail.com>
2023-03-01 12:28:33 +0530

    updated connection

--- backend/connection/connection.js
+++ backend/connection/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.set(`strictQuery`, true)
 
 async function getConnection() {
-    const uri = `mongodb+srv://Hussain:<hafiz0612>@cluster0.zgli815.mongodb.net/?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://Hussain:hafiz0612@cluster0.zgli815.mongodb.net/?retryWrites=true&w=majority`
     await mongoose.connect(uri, (err)=>{
         if(err){
             console.log("Connection to mongoDB failed");
