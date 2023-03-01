9742ed31af239dc1719e5b454cccb6d413593b0d
Hussain <hussainhafidh1@gmail.com>
2023-03-01 12:21:26 +0530

    added backend

---
+++ backend/connection/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+mongoose.set(`strictQuery`, true)
+
+async function getConnection() {
+    const uri = `mongodb+srv://Hussain:<hafiz0612>@cluster0.zgli815.mongodb.net/?retryWrites=true&w=majority`
+    await mongoose.connect(uri, (err)=>{
+        if(err){
+            console.log("Connection to mongoDB failed");
+        }
+        else{
+            console.log("Connected to MongoDB successfully");
+        }
+    })
+}
+module.exports = getConnection
